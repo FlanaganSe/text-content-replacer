5fbf76534622151fcac5627d94691036cd1dc3ec
Sean Flanagan <flanspan11@gmail.com>
2025-07-05 23:06:33 -0400

    chore: replace innerHtml with individually created elements to minimize risk

--- src/extension-popup.js
+++ src/extension-popup.js
@@ -3,35 +3,58 @@ const form = document.getElementById("form");
 const list = document.getElementById("list");
 
 const getItems = () => chrome.storage.local.get(KEY).then((d) => d[KEY] || []);
-const setItems = (items) => chrome.storage.local.set({ [KEY]: items });
+const setItems = (items) =>
+  chrome.storage.local.set({ [KEY]: items }).catch(console.error);
 
 // Render list
 const render = async () => {
   const items = await getItems();
-  const htmlItems = items.map((item, i) => {
-    return `<div class="item"><span class="find">${item.find}</span> → <span class=replace>${item.replace}</span><button class="delete" data-index="${i}"></button></div>`;
-  });
-  list.innerHTML = htmlItems.join("");
+  list.replaceChildren(
+    ...items.map((item, i) => {
+      const div = document.createElement("div");
+      div.className = "item";
+
+      // Find span
+      const findSpan = document.createElement("span");
+      findSpan.className = "find";
+      findSpan.textContent = item.find;
+      // Replace span
+      const replaceSpan = document.createElement("span");
+      replaceSpan.className = "replace";
+      replaceSpan.textContent = item.replace;
+      // Delete button
+      const deleteBtn = document.createElement("button");
+      deleteBtn.className = "delete";
+      deleteBtn.dataset.index = i;
+
+      // Append the HTML items to a div in the list
+      div.append(findSpan, " → ", replaceSpan, deleteBtn);
+      return div;
+    })
+  );
 };
 
-// Handle submit (add new item)
+// On submit new item handler
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
-  const find = form.find.value.trim();
-  const replace = form.replace.value.trim();
+  const data = new FormData(form);
+  const find = data.get("find")?.trim();
+  const replace = data.get("replace")?.trim();
 
   const items = await getItems();
+  if (!find || !replace) return; // Require find and replace fields
+  if (items.some((it) => it.find === find)) return; // Prevent duplicate find
+
   await setItems([...items, { find, replace }]);
   form.reset();
   render();
 });
 
-// Handle delete item
+// On delete item handler
 list.addEventListener("click", async (e) => {
-  if (!e.target.classList.contains("delete")) return;
+  if (!e.target.matches(".delete")) return;
   const items = await getItems();
-  items.splice(Number(e.target.dataset.index), 1);
-  await setItems(items);
+  await setItems(items.filter((_, i) => i !== +e.target.dataset.index));
   render();
 });
 
