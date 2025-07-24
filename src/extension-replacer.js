c7b24ab665bbb1d16e0ea3f06af0a3ed86961371
Sean Flanagan <flanspan11@gmail.com>
2025-07-24 17:37:47 -0400

    feat: use weakset to reduce risk of re-running node twice

--- src/extension-replacer.js
+++ src/extension-replacer.js
@@ -2,14 +2,22 @@ chrome.storage.local.get({ replacements: [] }).then(({ replacements }) => {
   if (!replacements.length) return;
 
   const SKIP = "input, textarea, [contenteditable], script, style";
+  const processedNodes = new WeakSet();
+
   const patterns = replacements.map(({ find, replace }) => {
     return [new RegExp(find.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"), "g"), replace];
   });
 
   const processNode = (node) => {
     if (node.nodeType === Node.TEXT_NODE && node.textContent?.trim()) {
+      if (processedNodes.has(node)) return;
+
       const text = patterns.reduce((text, [regex, replace]) => text.replace(regex, replace), node.textContent);
-      if (text !== node.textContent) node.textContent = text;
+
+      if (text !== node.textContent) {
+        processedNodes.add(node);
+        node.textContent = text;
+      }
     } else if (node.nodeType === Node.ELEMENT_NODE && !node.matches(SKIP)) {
       node.childNodes.forEach(processNode);
     }
